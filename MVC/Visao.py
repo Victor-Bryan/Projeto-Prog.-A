764a3a72c4ae044b9a143f1cc2bc810ae9dea37a
Alberto Campos <134335753+albertocampos2003@users.noreply.github.com>
2026-07-06 21:23:08 -0300

    Atualizar o Visao.py

---
+++ MVC/Visao.py
@@ -0,0 +1,32 @@
+from tkinter import *
+from tkinter import ttk
+
+class View:
+
+    def __init__(self):
+
+        self.root = Tk()
+        self.root.title("Editor de Desenhos")
+
+        self.frame = Frame(self.root)
+        self.frame.pack()
+
+        paddings = {'padx': 5, 'pady': 5}
+
+        ttk.Label(self.frame, text="Escolha a forma:" ).grid(row=0, column=0, sticky=W, **paddings,)
+        ttk.Label(self.frame, text="Cor da borda:" ).grid(row=1, column=0, sticky=W, **paddings)
+        ttk.Label(self.frame, text="Cor de preenchimento:").grid(row=2, column=0, sticky=W, **paddings)
+
+        self.tipo_figura_var = StringVar(value="Linha")
+        self.cor_borda_var = StringVar(value="black")
+        self.cor_preench_var = StringVar(value="")
+
+        ttk.OptionMenu(self.frame, self.tipo_figura_var, "Linha", "Linha", "Rabisco", "Retangulos","Ovais", "Circulos", "Triangulo", "Pentagono" ).grid(row=0, column=1, sticky=W, **paddings)
+
+        ttk.OptionMenu( self.frame, self.cor_borda_var, "black", "black", "white", "red", "blue","green", "orange", "yellow", "purple","pink" ).grid(row=1, column=1, sticky=W, **paddings)
+
+        ttk.OptionMenu(self.frame, self.cor_preench_var, "", "", "black", "white", "red", "blue","green", "orange", "yellow", "purple", "pink" ).grid(row=2, column=1, sticky=W, **paddings)
+
+        self.canvas = Canvas( self.frame, width=600, height=600, bg="white")
+
+        self.canvas.grid(row=3, column=0, columnspan=2, **paddings)

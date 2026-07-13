b20e1b44df881eb10d841e716c6ba610c2267164
Vbsantosl <vbzinho07@gmail.com>
2026-07-13 11:13:33 -0300

    Update Main.py

--- MVC/Main.py
+++ MVC/Main.py
@@ -1,3 +1,11 @@
+"""
+Módulo principal da aplicação.
+
+Este módulo é responsável por inicializar a interface gráfica (View),
+criar o controlador que conecta a View ao Model e iniciar o loop
+principal da aplicação.
+"""
+
 from Visao import View
 from Controlador import Controlador
 
